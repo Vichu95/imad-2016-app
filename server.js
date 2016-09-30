58fcebed8bf621f8d79df1a1180c502a08d14c92
Vishnudev K <k.vishnudev@gmail.com>
2016-09-30 19:24:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 
+app.get('/ui/pika', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'Pikachu_pokemon_wikipedia_thumb400x275'));
+});
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
